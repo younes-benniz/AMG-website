c0609095c2d4ea48e4a1cdc35bb521793ea321c3
younes-benniz <bennizyouness@gmail.com>
2023-07-18 23:23:19 +0100

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ export default {
 			header: ["Oswald", "sans-serif"],
 			body: ["Raleway", "sans-serif"],
 		},
-		extend: {},
+		extend: {
+			colors: {
+				deepBlue: "#004D7F", // header, navigaton, branding elements.
+				brightOrange: "#FF6600", //call-to-action buttons, highlighting special offers or booking options.
+				coolGray: "#888888", //sections, providing a neutral and modern backdrop for content.
+				freshGreen: "#0CCD43", // eco-friendly or hybrid vehicle labels, emphasizing sustainability or environmentally friendly options.
+				lightSilver: "#D3D3D3", // Accent color for borders, dividing sections, or subtle highlights
+				darkCharocoal: "#333333", // headings, titles, or important information, providing a strong contrast against lighter backgrounds.
+			},
+		},
 	},
 	plugins: [],
 };
