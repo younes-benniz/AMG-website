5d7227f77a59630bbad4ea0befeed94095c9aadf
younes-benniz <bennizyouness@gmail.com>
2023-07-19 22:50:03 +0100

    fix position of hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 function Hero() {
 	return (
-		<section className="flex w-screen sm:h-screen z-0 bg-hero-image bg-cover bg-center h-[calc(100vh_-_4rem)]">
+		<section className="flex sm:w-screen sm:h-screen z-0 bg-hero-image bg-cover sm:bg-center bg-no-repeat w-full bg-[top_right_-25rem] h-[calc(100vh_-_4rem)]">
 			<div className="relative flex sm:items-center items-start overflow-hidden bg-transparent sm:px-16 sm:pb-16 px-5 sm:mt-28 mt-20 mx-auto">
 				<div className="container relative flex flex-col backdrop-blur-sm bg-black/50 sm:p-12 p-10 sm:w-1/2 sm:rounded-br-full rounded-xl">
 					<motion.h1
