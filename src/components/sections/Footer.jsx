498a1cd50abe3a04ddecbf75848b91db96cc4dfc
younes-benniz <bennizyouness@gmail.com>
2023-08-23 08:07:20 +0100

    display git version

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -102,8 +102,36 @@ function Footer() {
 					</div>
 				</div>
 			</div>
-			<div className="py-6 text-sm font-body text-center text-gray-400">
+			<div className="flex flex-wrap gap-3 items-center justify-center py-6 text-sm font-body text-center text-gray-400">
 				© 1998 Company Co. All rights reserved.
+				<div className="flex items-center gap-2">
+					<svg
+						viewBox="0 0 32 32"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="#000000"
+						className="w-10 ">
+						<g id="SVGRepo_bgCarrier" strokeWidth={0} />
+						<g
+							id="SVGRepo_tracerCarrier"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+						/>
+						<g id="SVGRepo_iconCarrier">
+							<title>file_type_git</title>
+							<path
+								d="M29.472,14.753,17.247,2.528a1.8,1.8,0,0,0-2.55,0L12.158,5.067l3.22,3.22a2.141,2.141,0,0,1,2.712,2.73l3.1,3.1a2.143,2.143,0,1,1-1.285,1.21l-2.895-2.895v7.617a2.141,2.141,0,1,1-1.764-.062V12.3a2.146,2.146,0,0,1-1.165-2.814L10.911,6.314,2.528,14.7a1.8,1.8,0,0,0,0,2.551L14.753,29.472a1.8,1.8,0,0,0,2.55,0L29.472,17.3a1.8,1.8,0,0,0,0-2.551"
+								style={{ fill: "#dd4c35" }}
+							/>
+							<path
+								d="M12.158,5.067l3.22,3.22a2.141,2.141,0,0,1,2.712,2.73l3.1,3.1a2.143,2.143,0,1,1-1.285,1.21l-2.895-2.895v7.617a2.141,2.141,0,1,1-1.764-.062V12.3a2.146,2.146,0,0,1-1.165-2.814L10.911,6.314"
+								style={{ fill: "#fff" }}
+							/>
+						</g>
+					</svg>
+					<p className="text-sm font-bold tracking-wider font-body text-white">
+						{import.meta.env.VITE_GIT_VERSION}
+					</p>
+				</div>
 			</div>
 		</footer>
 	);
